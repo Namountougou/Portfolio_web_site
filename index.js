e2d0804c8aa85b4c74efd7421708ce3f17d12afe
Namountougou <naboure@gmail.com>
2023-01-17 17:10:07 +0000

    ok

--- index.js
+++ index.js
@@ -16,3 +16,84 @@ document.onscroll = () => {
     else {
         document.querySelector('.header').classList.remove('active');
     }}
+
+    // testimonial
+
+    const slider = function() {
+        const slides = document.querySelectorAll(".slide");
+        const btnLeft = document.querySelector(".slider-btn-left");
+        const btnRight = document.querySelector(".slider-btn-right");
+        const dotContainer = document.querySelector(".dots");
+
+        let curSlide = 0;
+        const maxSlide = slides.length;
+
+        //funtion
+        const createDots = function () {
+            slides.forEach(function (_, i){
+                dotContainer.insertAdjacentHTML("beforeend", `<button class="dots-dot" data-slide="${i}"></button>`);
+            });
+        };
+
+
+        const activeDot = function (slide) {
+            document.querySelectorAll(".dots-dot").forEach((dot) => dot.classList.remove("dots-dot-active"));
+
+            document.querySelector(`.dots-dot[data-slide="${slide}"]`).classList.add("dots-dot-active");
+        };
+
+        const goToSlide = function (slide) {
+            slides.forEach(
+                (s, i) => (s.style.transform = ` translateX(${100 * (i - slide)}%)`)
+            );
+        };
+
+        const nextSlide = function (){
+            if(  curSlide === maxSlide -1 ){
+                curSlide = 0;
+            }
+            else {
+                curSlide++;
+            }
+            goToSlide(curSlide);
+            activeDot(curSlide);
+        };
+
+        const preSlide = function (){
+            if (curSlide === 0){
+                curSlide = maxSlide -1;
+            } else {
+                curSlide--;
+            }
+            goToSlide(curSlide);
+            activeDot(curSlide);
+
+        };
+
+        const init = function(){
+            goToSlide(0);
+            createDots();
+
+            activeDot(0);
+        };
+        init();
+
+        // event handler
+
+        btnRight.addEventListener("click", nextSlide);
+        btnLeft.addEventListener("click", preSlide);
+
+        document.addEventListener("keydown", function (e){
+            if(this.elementFromPoint.key === "ArrowLeft") preSlide();
+            e.key === "ArrowRight" && nextSlide();
+        });
+
+        dotContainer.addEventListener("click", function(e){
+            if(e.target.classList.contains("dots-dot")){
+                const {slide} = e.target.dataset;
+                goToSlide(slide);
+                activeDot(slide);
+            }
+        });
+    };
+    slider();
